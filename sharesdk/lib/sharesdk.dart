91ee5128deb7f1a5973adec7e1bc27e313ea880e
lishuzhi1121 <lishuzhi1121@163.com>
2019-08-15 16:18:22 +0800

    调整项目结构

--- sharesdk/lib/sharesdk.dart
+++
@@ -1,9 +0,0 @@
-/// Flutter plugin implementing share and auth
-///
-/// To use, import `package:sharesdk/sharesdk.dart`;
-library sharesdk;
-
-export './sharesdk_defines.dart';
-export './sharesdk_interface.dart';
-export './sharesdk_map.dart';
-export './sharesdk_register.dart';
