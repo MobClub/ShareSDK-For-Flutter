0f3ab31b26f61d22e12f0ad6ab8fb66fe6c2f743
林创湖 <linchuanghu@addcn.com>
2020-01-21 17:55:35 +0800

    Migrate to androidx and upgrade compileSdkVersion

--- sharesdk_plugin/example/ios/Flutter/flutter_export_environment.sh
+++ sharesdk_plugin/example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\FLUTTER\sdk\flutter_windows_v1.0.0-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\GitLab\flutter\sharesdk-for-flutter\sharesdk_plugin\example"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutterDemo\ShareSDK-For-Flutter\sharesdk_plugin\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\FLUTTER\sdk\flutter_windows_v1.0.0-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
