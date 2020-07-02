c57d18e89d6f2cb747bc2618a55a485bb92ef092
moblarry <lipf@yoozoo.com>
2020-07-02 09:43:40 +0800

    update v4.3.9

--- sharesdk_plugin/example/lib/home.dart
+++ sharesdk_plugin/example/lib/home.dart
@@ -125,9 +125,9 @@ class _HomePageState extends State<HomePage> {
           "title",
           "text",
           [
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541565611543&di=4615c8072e155090a2b833059f19ed5b&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201501%2F06%2F20150106003502_Ajcte.jpeg"
+            "http://download.sdk.mob.com/web/images/2019/07/30/14/1564468183056/750_750_65.12.png"
           ],
-          "http://pic28.photophoto.cn/20130818/0020033143720852_b.jpg",
+          "http://download.sdk.mob.com/web/images/2019/07/30/14/1564468183056/750_750_65.12.png",
           null,
           "http://pic28.photophoto.cn/20130818/0020033143720852_b.jpg",
           "http://pic28.photophoto.cn/20130818/0020033143720852_b.jpg",
